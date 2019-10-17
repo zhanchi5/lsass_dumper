908d6df4a1fa692dcd5da207fe50198c3ac4fbee
zhanchi5 <zhanchi5@protonmail.com>
2019-10-17 19:05:30 +0300

    added requirements

--- configuration.py
+++ configuration.py
@@ -1,4 +1,2 @@
-protocol_path="./CrackMapExec/cme/protocols/smb.py"
-protocol_db_path = "./CrackMapExec/cme/protocols/smb/database.py"
 src_x32 = "./binaries/procdump.exe"
 src_x64 = "./binaries/procdump64.exe"
